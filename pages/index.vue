ad33bb65bd69df2a9c7b8600fa8de4b681326728
linkers17 <romeo-gry@mail.ru>
2025-08-11 23:41:31 +0700

    fetch data (2)

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,32 @@
 <template>
-
+	<div class="posts">
+		<div
+			v-if="posts"
+			class="posts__list"
+		>
+			<PostCard
+				v-for="post in posts.posts"
+				:key="post.id"
+				:post
+			/>
+		</div>
+	</div>
 </template>
 
 <script lang="ts" setup>
+import type { PostsResponse } from '~/types/post.types'
 
+const config = useRuntimeConfig()
+const API_URL = config.public.apiurl
+
+const { data: posts } = await useFetch<PostsResponse>(`${API_URL}/posts`)
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.posts__list {
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	gap: 38px;
+}
+</style>
